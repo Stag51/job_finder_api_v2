5218df70853d236c148537c71cf3832f985022d4
Stag51 <dev.saad.shabbir@gmail.com>
2025-04-05 18:59:06 +0500

    Uploading project files

--- app/config.py
+++ app/config.py
@@ -3,4 +3,4 @@
 
 # load_dotenv()
 
-# OPENAI_API_KEY = os.getenv("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")
+# OPENAI_API_KEY = os.getenv("")
